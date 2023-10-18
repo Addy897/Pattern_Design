9612ff63d1f915de8e7bad5b13d1bd6b791fb283
Addy897 <145797684+Addy897@users.noreply.github.com>
2023-10-18 21:29:22 +0530

    Add files via upload

---
+++ pattern_alpha1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x = 96;
+    int num=18;
+    for(int i=1;i<=num;i++){
+        for(int j=1;j<=(num*2-i*2);j++)
+	        printf("-");
+        for(int k=0;k<i;k++){
+            if(i-1==0){
+                printf("%c",x+num-k);
+                continue;
+        
+            }
+    	    printf("%c-",x+num-k);
+        }
+        for(int l=i-1;l>0;l--){
+            if(l-1==0){
+                printf("%c",x+num-l+1);
+                continue;
+            }
+    	    printf("%c-",x+num-l+1);
+            
+            
+        }
+        for(int j=1;j<=(num*2-i*2);j++)
+	        printf("-");
+        printf("\n");
+        
+    }
+    for(int i=num-1;i>0;i--){
+        for(int j=1;j<(num*2-i*2+1);j++)
+	        printf("-");
+        for(int j=0;j<i;j++)
+    	    printf("%c-",x+num-j);
+        for(int j=i-1;j>0;j--){
+            
+    	    printf("%c",x+num-j+1);
+            if((num*2-i*2+1)>=1){
+                printf("-");
+            }
+        }
+        for(int j=1;j<(num*2-i*2);j++)
+	        printf("-");
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
